888001360ed43b4e22f29ba690061fc15d3112c5
ABIGAIL HARRISON-NYE <abigail@giffgaff.co.uk>
2025-05-25 22:57:46 +0100

    wip: Add sort logic

--- src/pages/EscapeRooms/index.js
+++ src/pages/EscapeRooms/index.js
@@ -1,16 +1,48 @@
+import { useState } from 'react'
 import { Card, MainContent, Title } from '../../components'
 import * as Styled from './EscapeRooms.styled'
 import escapeRooms from '../../data/escape-rooms.json'
+import { convertRatingsToOverall } from '../../utils'
 
 function EscapeRooms() {
+  const [sortOrder, setSortOrder] = useState('newest')
+
+  function getOverall(room) {
+    const overall = convertRatingsToOverall(
+      room.ourEscape?.ratings || {},
+      false
+    )
+    return overall
+  }
+
+  const sortedRooms = [...escapeRooms].sort((firstRoom, secondRoom) => {
+    if (sortOrder === 'highest') {
+      return getOverall(secondRoom) - getOverall(firstRoom)
+    }
+    if (sortOrder === 'lowest') {
+      return getOverall(firstRoom) - getOverall(secondRoom)
+    }
+    const firstDate = new Date(firstRoom.ourEscape?.datePlayed)
+    const secondDate = new Date(secondRoom.ourEscape?.datePlayed)
+    if (sortOrder === 'newest') {
+      return secondDate - firstDate
+    }
+    return firstDate - secondDate
+  })
+
   return (
     <MainContent>
       <Title>Escape Rooms</Title>
-      <p>
-        Sort by: highest rated, lowest rated, newest to oldest, oldest to newest
-      </p>
+      <p>{escapeRooms.length} escape rooms found</p>
+      <p>Sort by:</p>
+      <select value={sortOrder} onChange={e => setSortOrder(e.target.value)}>
+        <option value='newest'>Newest to Oldest</option>
+        <option value='oldest'>Oldest to Newest</option>
+        <option value='highest'>Highest Rated</option>
+        <option value='lowest'>Lowest Rated</option>
+      </select>
       <Styled.Grid>
-        {Object.values(escapeRooms).map(room => (
+        {sortedRooms.map(room => (
           <a href={`/escape-rooms/room-info?roomId=${room.id}`} key={room.id}>
             <Card imgSrc={room.image} title={room.name}>
               {room.description}
--- src/utils/convertRatingsToOverall.js
+++ src/utils/convertRatingsToOverall.js
@@ -1,15 +1,24 @@
-const convertRatingsToOverall = ratings => {
+const convertRatingsToOverall = (ratings, percentage = true) => {
   if (!ratings || typeof ratings !== 'object') {
-    return '0%'
+    if (percentage) return '0%'
+    return 0
   }
 
   const values = Object.values(ratings)
 
-  if (values.length === 0) return '0%'
+  if (values.length === 0) {
+    if (percentage) {
+      return '0%'
+    }
+    return 0
+  }
 
   let overallRating = values.reduce((sum, rating) => sum + Number(rating), 0)
   overallRating = overallRating / (values.length * 10)
-  overallRating = overallRating * 100 + '%'
+  overallRating = overallRating * 100
+  if (percentage) {
+    overallRating = overallRating + '%'
+  }
   return overallRating
 }
 
